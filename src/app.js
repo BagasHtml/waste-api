a349493c7b841000f5478c5602ddc938bf609534
BagasHtml <ida.indawati@gmail.com>
2026-07-13 05:58:07 +0700

    add ratelimiter

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import predictRouter from './routes/predict.routes.js';
 import { getStatus } from './controllers/status.controller.js';
+import { statusRateLimiter } from './middlewares/rateLimiter.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/status', getStatus);
+app.get('/status', getStatus, statusRateLimiter);
 
 app.use('/api/v1', predictRouter);
 
--- src/routes/predict.routes.js
+++ src/routes/predict.routes.js
@@ -4,15 +4,16 @@ import { getAlerts } from '../controllers/alert.controller.js';
 import { getNews } from '../controllers/news.controller.js';
 import { getAutopilot } from '../controllers/autopilot.controller.js'
 import { validateApiKey } from '../middlewares/auth.js';
+import { aiRateLimiter, generalRateLimiter } from '../middlewares/rateLimiter.js';
 
 const router = Router();
 
 router.use(validateApiKey);
 
-router.get('/news', getNews);
-router.get('/autopilot', getAutopilot);
-router.post('/predict', corePredict);
-router.post('/predict/csv', exportCSV);
-router.get('/alerts', getAlerts);
+router.get('/news', getNews, generalRateLimiter);
+router.get('/autopilot', getAutopilot, aiRateLimiter);
+router.post('/predict', corePredict, aiRateLimiter);
+router.post('/predict/csv', exportCSV, aiRateLimiter);
+router.get('/alerts', getAlerts, generalRateLimiter);
 
 export default router;
